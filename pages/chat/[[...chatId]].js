baa931d59f4b7117e031478800fb5c9590faecfa
Pedro Viana <pedroviana.developer13@gmail.com>
2024-05-05 16:47:36 -0300

    feat: added logic to generate response, but do not generate yet

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,14 +1,33 @@
 import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
+import { streamReader } from "openai-edge-stream";
 import { useState } from "react";
 
 export default function ChatPage() {
+    const [incomingMessage, setIncomingMessage] = useState("");
     const [messageText, setMessageText] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("Message Text: ",messageText)
-    }
+        console.log("Message Text: ",messageText);
+        const response = await fetch(`/api/chat/sendMessage`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({message: messageText}),
+        });
+        const data = response.body;
+        if(!data){
+            return;
+        }
+
+        const reader = data.getReader();
+        await streamReader(reader, (message) => {
+            console.log("Message: ", message);
+            setIncomingMessage(s => `${s}${message.content}`);
+        });
+    };
 
     return (
         <>
@@ -18,7 +37,9 @@ export default function ChatPage() {
             <div className="grid h-screen grid-cols-[260px_1fr]">
                 <ChatSidebar />
                 <div className="bg-gray-700 flex flex-col">
-                    <div className="flex-1">chat window</div>
+                    <div className="flex-1 text-white">
+                        {incomingMessage}
+                    </div>
                     <footer className="bg-gray-800 p-10">
                         <form onSubmit={handleSubmit}>
                             <fieldset className="flex gap-2">
