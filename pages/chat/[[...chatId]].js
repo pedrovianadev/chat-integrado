2a9346a31d7ca2e09e859699ceb564431e86401c
Pedro Viana <pedroviana.developer13@gmail.com>
2024-05-05 16:10:16 -0300

    feat: forms now works ad have the style of footer

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,18 +1,36 @@
+import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+    const [messageText, setMessageText] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Message Text: ",messageText)
+    }
+
     return (
         <>
             <Head>
-                <title>Next Chat</title>
+                <title>New Chat</title>
             </Head>
             <div className="grid h-screen grid-cols-[260px_1fr]">
-                <div>
-                    Sidebar
-                </div>
+                <ChatSidebar />
                 <div className="bg-gray-700 flex flex-col">
                     <div className="flex-1">chat window</div>
-                    <footer className="bg-gray-800 p-10">footer</footer>
+                    <footer className="bg-gray-800 p-10">
+                        <form onSubmit={handleSubmit}>
+                            <fieldset className="flex gap-2">
+                                <textarea 
+                                value={messageText}
+                                onChange={e => setMessageText(e.target.value)}
+                                placeholder="Send a message..."
+                                className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"/>
+                                <button type="submit" className="btn">Send</button>
+                            </fieldset>
+                        </form>
+                    </footer>
                 </div>
             </div>
         </>
